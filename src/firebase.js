c4850bd2411b3060e48a7cb6d459dd1a79fe0be8
Eric <eric.elserafy@ryerson.ca>
2021-10-15 18:47:37 -0400

    Added firebase and react router

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUnppA-cGuNDRZhZUHmO2hDLj01QjZGaM",
+    authDomain: "strife-4c89f.firebaseapp.com",
+    projectId: "strife-4c89f",
+    storageBucket: "strife-4c89f.appspot.com",
+    messagingSenderId: "667447039660",
+    appId: "1:667447039660:web:eb4431a0c0654ed5b87109"
+  };
