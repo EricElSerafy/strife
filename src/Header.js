e0260e00ae5fd11454a8c22119a2cefe01e98443
Eric <eric.elserafy@ryerson.ca>
2021-10-16 17:11:33 -0400

    Created header sections and imported styled components

---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components"
+
+
+
+function Header(){
+    return (
+        <HeaderContainer>
+            {/* Header left */}
+            <HeaderLeft>
+
+            </HeaderLeft>
+            {/* Header middle */}
+            <HeaderMiddle>
+
+            </HeaderMiddle>
+            {/* Header right */}
+            <HeaderRight>
+
+            </HeaderRight>
+        </HeaderContainer>
+        
+    )
+    
+}
+
+export default Header;
+const HeaderContainer = styled.div``
+const HeaderLeft = styled.div``
+const HeaderMiddle = styled.div``
+const HeaderRight = styled.div``
